635620cae29c4679537cbd7ce84427b5e25f12a0
Quentin Soucy <qdsoucy@gmail.com>
2024-11-16 16:44:38 -0600

    Updated student/main.js

--- student/main.js
+++ student/main.js
@@ -7,17 +7,38 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const geometry = new THREE.CircleGeometry(2);
+const green = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const circle = new THREE.Mesh( geometry, green );
+scene.add( circle );
+
+
+const g2 = new THREE.CircleGeometry(0.25);
+const blue = new THREE.MeshBasicMaterial( {color: 0x0000ff});
+const eye = new THREE.Mesh(g2, blue);
+const eye2 = new THREE.Mesh(g2, blue);
+scene.add(eye);
+scene.add(eye2);
+
+const g3 = new THREE.CircleGeometry(0.5, 32, Math.PI, Math.PI);
+const red = new THREE.MeshBasicMaterial( {color: 0xff0000});
+const mouth = new THREE.Mesh(g3, red)
+scene.add(mouth)
+
+moveTo(eye, 0.5, 0.7, 2);
+moveTo(eye2, -0.5, 0.7, 2);
+moveTo(mouth, 0, 0.2, 2);
+
+function moveTo(obj, x, y, z) {
+	obj.position.x = x
+	obj.position.y = y
+	obj.position.z = z
+}
 
 camera.position.z = 5;
 
 function animate() {
 
-	cube.rotation.z += 0.01
-	cube.rotation.y += 0.01
 	renderer.render( scene, camera );
 	
 }
