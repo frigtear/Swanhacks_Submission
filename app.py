ef4f50acbe6678bf3d77f8e2b6e2b45e6646dacf
Grace <124198528+grlefl@users.noreply.github.com>
2024-11-16 14:37:11 -0600

    basic backend communication achieved

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+# Variable to hold the current image state
+current_image = "image1.jpg"
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/viewer')
+def viewer():
+    return render_template('viewer.html')
+
+
+# SocketIO event to handle image change
+@socketio.on('change_image')
+def handle_change_image(data):
+    global current_image
+    current_image = data['image']  # Update the current image
+    emit('update_image', {'image': current_image}, broadcast=True)  # Broadcast to all clients
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
