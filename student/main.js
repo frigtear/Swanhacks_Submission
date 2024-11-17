b6fb894b41f1f219a3a3387b489a270ca70f72b9
Emmy Liu <98995338+emmy-liu@users.noreply.github.com>
2024-11-16 23:56:13 -0600

    She's 3d now!!! :D

--- student/main.js
+++ student/main.js
@@ -8,19 +8,19 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.SphereGeometry(2);
-const green = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const green = new THREE.MeshStandardMaterial( { color: 0x00ff00 } );
 const sphere = new THREE.Mesh( geometry, green );
 scene.add( sphere );
 
 const g2 = new THREE.SphereGeometry(0.25);
-const blue = new THREE.MeshBasicMaterial( {color: 0x0000ff});
+const blue = new THREE.MeshStandardMaterial( {color: 0x0000ff});
 const eye = new THREE.Mesh(g2, blue);
 const eye2 = new THREE.Mesh(g2, blue);
 scene.add(eye);
 scene.add(eye2);
 
 const straight_mouth = new THREE.BoxGeometry(1.3, 0.1, 0.1);
-const red = new THREE.MeshBasicMaterial( {color: 0xff0000});
+const red = new THREE.MeshStandardMaterial( {color: 0xff0000});
 const smile_mouth = new THREE.SphereGeometry(0.5, 32, 16, 0, Math.PI*2, Math.PI/2, Math.PI);
 const frown_mouth = new THREE.SphereGeometry(-0.5, -32, -16, 0, Math.PI*2, Math.PI/2, Math.PI);
 const mouth = new THREE.Mesh(straight_mouth, red);
@@ -53,15 +53,15 @@ function animate() {
 }
 
 function addLighting(scene) {
-    let color = 0xFFFFFF;
-    let intensity = 1;
-    let light = new THREE.DirectionalLight(color, intensity);
-    light.position.set(0, 2, 0);
-    light.target.position.set(-2, 0, -2);
+    const color = 0xffffff;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(2, 2, 5);
     scene.add(light);
-    scene.add(light.target);
+  
+    const ambientLight = new THREE.AmbientLight(0x404040);
   }
-
+  
   addLighting(scene);
 
-renderer.setAnimationLoop( animate, addLighting );
+renderer.setAnimationLoop(animate);
